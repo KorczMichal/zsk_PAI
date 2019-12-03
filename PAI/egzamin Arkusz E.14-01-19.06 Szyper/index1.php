60ed9fe497e50f5cc73e3b385db6eabb721a2896
KorczMichal <korczmichal09@gmail.com>
2019-12-03 11:20:33 +0100

    1

---
+++ PAI/egzamin Arkusz E.14-01-19.06 Szyper/index1.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<?php
+  $sql = "SELECT * FROM `towary` WHERE promocja= 1";
+
+  $connect= mysqli_connect("localhost",'root',"",'sklep_MK');
+  mysqli_set_charset($connect,"utf8");
+  $result= mysqli_query($connect,$sql);
+echo "<ul>";
+  while($row = mysqli_fetch_assoc($result)){
+    echo '<li>',$row["nazwa"],'</li>';
+  }
+echo "<ul>";
+  mysqli_close($connect);
+?>
+
+<form method="post">
+  <select name="towar">
+    <option value="c">Czekolada</option>
+    <option value="g">Grześki</option>
+    <option value="b">Baton</option>
+  </select>
+  <button type="submit" name="button">WYBIERZ</button>
+</form>
+<?php
+if (isset($_POST["towar"])) {
+switch ($_POST['towar']) {
+  case 'c':
+  $towar="Czekolada";
+    break;
+    case 'g':
+    $towar="Grześki";
+      break;
+      case 'b':
+      $towar="Baton";
+        break;
+
+}
+echo $towar;
+$connect= mysqli_connect('localhost','root',"","sklep_MK");
+$sql= "SELECT cena FROM `towary` WHERE nazwa = '$towar'";
+$result = mysqli_query($connect,$sql);
+$row=mysqli_fetch_assoc($result);
+$promocja = round($row['cena']*0.85,2);
+echo " ",$promocja;
+mysqli_close($connect);}
+
+
+
+
+
+ ?>
+  </body>
+</html>
